a7ee15a9dbd7331e09a7be55593ebebb9e9de9c4
fiza11 <fizahusain1110@gmail.com>
2020-07-13 11:44:16 +0530

    Updates

--- src/pages/week3.js
+++ src/pages/week3.js
@@ -11,7 +11,7 @@ const Week1 = () => (
         <p>This week I learnt mainly about CUDA streams. I learnt how to transfer data efficiently between the host and device and and how to overlap data transfers in CUDA.</p>
         <ul>
             <li>
-                Initially, I tried using <code>Thrust</code>,  a CUDA C++ template library to implement parallel operations but it was useless. I proposed an idea to use non-default streams for issuing various kernels to different operations for parallel execution. The idea was to use <code>n</code> streams, allocate pinned memory and then perform asynchronous memory copy/fill on each stream to fill <code>n</code> vectors. 
+                Initially, I tried using <code>Thrust</code>,  a CUDA C++ template library to fill <code>n</code> vectors parallely but it was useless. I proposed an idea to use non-default streams for issuing various kernels to different operations for parallel execution. The idea was to use <code>n</code> streams, allocate pinned memory and then perform asynchronous memory copy/fill on each stream to fill <code>n</code> vectors. 
             </li>
             <li>
                 I tried using lesser number of streams and then filling <code>n</code> vectors using some round robin algorithms but couldn't propose the API to support this idea. 
